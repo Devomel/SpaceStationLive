9178c19388dbb323bace2bc969c995592dbfaf3d
Devomel <omellwork@gmail.com>
2024-07-28 14:46:29 +0300

    remove second rerender from rtk query

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,10 +1,13 @@
 import { MapContainer, Marker, TileLayer } from "react-leaflet";
+import 'leaflet/dist/leaflet.css';
 import { MapView } from "./MapView";
 import { FC, ReactNode } from "react";
 import { LatLngTuple } from "leaflet";
 import { issSlice } from "../services/StationService";
 import { Time } from "./Time";
 import { LocationText } from "./LocationText";
+import "../styles/map.css"
+
 
 export type LocationType = LatLngTuple
 export interface IMapProps {
@@ -13,8 +16,15 @@ export interface IMapProps {
 
 export const Map: FC<IMapProps> = ({ children }) => {
   const { data: location, error, isLoading } = issSlice.useFetchLocationISSQuery("", {
+    selectFromResult: ({ data, error, isLoading }) => ({
+      data,
+      error,
+      isLoading,
+    }),
     pollingInterval: 5000
   })
+
+  console.log("render map")
   if (isLoading) return <div>Loading...</div>;
   if (error) return <div>Error occurred</div>;
   if (location && location[2])
