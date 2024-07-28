bb3d876e9874e7183efcef59cdda04ff31a5ec8f
Devomel <omellwork@gmail.com>
2024-07-28 15:00:01 +0300

    revive typed hooks

--- src/store/store.ts
+++ src/store/store.ts
@@ -9,3 +9,5 @@ export const store = configureStore({
     getDefaultMiddleware().concat(issSlice.middleware)
 })
 
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
